09494ba6b061a33a205ed3fc8b20e30398397820
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-02 20:44:22 +0530

    Added DbContext as a service in program.cs

--- CitiesManager.WebAPI/Program.cs
+++ CitiesManager.WebAPI/Program.cs
@@ -1,8 +1,17 @@
+using CitiesManager.WebAPI.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+//Adding DbContext as a service to the IoC container
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    });
 
 var app = builder.Build();
 
