4834cb084fe475559e2386c17841bf9dd8aa8093
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-02 23:46:36 +0530

    Created CustomControllerBase as child of ControllerBase class so that this class can be used as parent for remaining controllers

---
+++ CitiesManager.WebAPI/Controllers/CustomControllerBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CitiesManager.WebAPI.Controllers
+{
+    [Route("api/[controller]")] 
+    [ApiController]
+    public class CustomControllerBase : ControllerBase //CustomControllerBase will be parent class to rest of WebApi Controllers, which don't require use of [Route()] & [ApiController] attributes any
+    {
+    }
+}
