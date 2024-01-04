2eb79b63c008c520ab7e8b6dea497e7248aed21b
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-05 00:38:02 +0530

    Revert back to use version provided as Route parameter in CustomController.cs

--- CitiesManager.WebAPI/Controllers/CustomControllerBase.cs
+++ CitiesManager.WebAPI/Controllers/CustomControllerBase.cs
@@ -2,8 +2,8 @@
 
 namespace CitiesManager.WebAPI.Controllers
 {
-    //[Route("api/v{version:apiVersion}/[controller]")] //Version is specified as request url route parameter
-    [Route("api/[controller]")] //version is specified as query string
+    [Route("api/v{version:apiVersion}/[controller]")] //Version is specified as request url route parameter
+    //[Route("api/[controller]")] //version is specified as query string
     [ApiController]
     public class CustomControllerBase : ControllerBase //CustomControllerBase will be parent class to rest of WebApi Controllers, which don't require use of [Route()] & [ApiController] attributes any
     {
