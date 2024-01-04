884317fdcbfa8e2ec8904fdc4b91d37f9e69916a
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-05 00:40:47 +0530

    Modifed Program.cs to use SwaggerDocs & enable Swagger Endpoints for individual versions & adding VersionedApiExplorer to group them based on version format provided such that SwaggerUI looks good

--- CitiesManager.WebAPI/Program.cs
+++ CitiesManager.WebAPI/Program.cs
@@ -2,6 +2,7 @@ using CitiesManager.WebAPI.DatabaseContext;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,9 +19,11 @@ builder.Services.AddControllers(
 builder.Services.AddApiVersioning(
     config =>
     {
-        //config.ApiVersionReader = new UrlSegmentApiVersionReader(); //to identify current working version of api from request url (mentioned in CustomController Route as route parameter)
+        config.ApiVersionReader = new UrlSegmentApiVersionReader(); //to identify current working version of api from request url (mentioned in CustomController Route as route parameter)
         //config.ApiVersionReader = new QueryStringApiVersionReader(); //to identify current working version of api from query string 'api-version' (mentioned in CustomController)
-        config.ApiVersionReader = new HeaderApiVersionReader("api-version"); //to identify current working version of api from request header 'api-version' (Make get request with key 'api-version' value 1 in postman)
+        //config.ApiVersionReader = new HeaderApiVersionReader("api-version"); //to identify current working version of api from request header 'api-version' (Make get request with key 'api-version' value 1 in postman)
+
+
         config.DefaultApiVersion = new ApiVersion(1, 0); //setting default api version to 1.0 if no version is specified in querystring
         config.AssumeDefaultVersionWhenUnspecified = true;
     });
@@ -40,7 +43,29 @@ builder.Services.AddSwaggerGen(
         options.IncludeXmlComments(
             Path.Combine(AppContext.BaseDirectory, "api.xml")
             );
+
+        //writing swagger documentations for the two versions
+        //2. supplying versions explicitly using the below SwaggerDoc's
+        options.SwaggerDoc("v1", new OpenApiInfo()
+        {
+            Title = "Cities Web API",
+            Version = "1.0"
+        });
+
+        options.SwaggerDoc("v2", new OpenApiInfo()
+        {
+            Title = "Cities Web API",
+            Version = "2.0"
+        });
+
     }); //configure swagger to generate documentation for API's endpoints (openapi specification).
+
+builder.Services.AddVersionedApiExplorer(options =>
+{
+    //3. letting swagger substitute to corresponding swagger version path.
+    options.GroupNameFormat = "'v'VVV"; //eg:v1
+    options.SubstituteApiVersionInUrl = true; //when set true then the version number above can be substituted in SwaggerEndPoint options provided
+});
 
 var app = builder.Build();
 
@@ -50,7 +75,13 @@ app.UseHsts();
 app.UseHttpsRedirection();
 
 app.UseSwagger(); //creates endpoint for swagger.json file (openapi specification abt web api controller endpoints)
-app.UseSwaggerUI(); //creates swagger UI for testing all web api controllers / action methods
+app.UseSwaggerUI(options =>
+{
+    //providing two seperate endpoints for locating swagger.json based on the versions requested by the user.
+    //1.enabling end points
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "1.0");
+    options.SwaggerEndpoint("/swagger/v2/swagger.json", "2.0");
+}); //creates swagger UI for testing all web api controllers / action methods
 
 app.UseAuthorization();
 
