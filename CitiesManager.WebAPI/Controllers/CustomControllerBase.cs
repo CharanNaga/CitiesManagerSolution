7ecff2fb24797400d5af7e83d35176a82ab4e85a
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-04 23:49:18 +0530

    Modifed Route to include versionApi in CustomControllerBase

--- CitiesManager.WebAPI/Controllers/CustomControllerBase.cs
+++ CitiesManager.WebAPI/Controllers/CustomControllerBase.cs
@@ -2,7 +2,7 @@
 
 namespace CitiesManager.WebAPI.Controllers
 {
-    [Route("api/[controller]")] 
+    [Route("api/v{version:apiVersion}/[controller]")] 
     [ApiController]
     public class CustomControllerBase : ControllerBase //CustomControllerBase will be parent class to rest of WebApi Controllers, which don't require use of [Route()] & [ApiController] attributes any
     {
