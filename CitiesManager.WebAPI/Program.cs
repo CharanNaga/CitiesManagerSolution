d874ce8228e568e0f74a0c618fcc27da4b6a1496
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-04 22:41:27 +0530

    Enabled Swagger Services & used Swagger middlewares in Program.cs

--- CitiesManager.WebAPI/Program.cs
+++ CitiesManager.WebAPI/Program.cs
@@ -13,12 +13,19 @@ builder.Services.AddDbContext<ApplicationDbContext>(
         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
     });
 
+//add swagger as service
+builder.Services.AddEndpointsApiExplorer(); //enables swagger to read metadata(http methods, url attributes) of endpoints (web api action methods) i.e., generates description of all endpoints
+builder.Services.AddSwaggerGen(); //configure swagger to generate documentation for API's endpoints (openapi specification).
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseHsts();
 
 app.UseHttpsRedirection();
+
+app.UseSwagger(); //creates endpoint for swagger.json file (openapi specification abt web api controller endpoints)
+app.UseSwaggerUI(); //creates swagger UI for testing all web api controllers / action methods
 
 app.UseAuthorization();
 
