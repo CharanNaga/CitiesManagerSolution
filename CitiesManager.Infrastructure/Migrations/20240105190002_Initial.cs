e6535eff148b18e40b02d534cc34463a1919372b
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-06 00:44:32 +0530

    Code-First Migrations were done in Infrastructure Project

---
+++ CitiesManager.Infrastructure/Migrations/20240105190002_Initial.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CitiesManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    CityID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CityName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.CityID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "CityID", "CityName" },
+                values: new object[,]
+                {
+                    { new Guid("1a81ec9e-dd06-4c90-96d3-d3ef2b9217a6"), "Milan" },
+                    { new Guid("99737f36-84fc-412b-ba0f-f592bf0d2257"), "London" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
