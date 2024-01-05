aede88266702ba46984758f4d595e370b95d4873
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-06 00:41:28 +0530

    Created DatabaseContext folder & moved previously situated ApplicationDbContext into it with change in namespace

---
+++ CitiesManager.Infrastructure/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using CitiesManager.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CitiesManager.Infrastructure.DatabaseContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public virtual DbSet<City> Cities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Adding Entity with a Table Name
+            modelBuilder.Entity<City>().ToTable("Cities");
+
+            //Adding Seed Data
+            modelBuilder.Entity<City>().HasData(
+                new City()
+                {
+                    CityID = Guid.Parse("99737F36-84FC-412B-BA0F-F592BF0D2257"),
+                    CityName = "London"
+                });
+
+            modelBuilder.Entity<City>().HasData(
+               new City()
+               {
+                   CityID = Guid.Parse("1A81EC9E-DD06-4C90-96D3-D3EF2B9217A6"),
+                   CityName = "Milan"
+               });
+
+        }
+    }
+}
