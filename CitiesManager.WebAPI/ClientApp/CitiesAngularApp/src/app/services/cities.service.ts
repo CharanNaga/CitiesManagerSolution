11dcd209e4b5390e8c3ced5b7f53d24f38dee8af
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-05 19:41:17 +0530

    Modified cities.service.ts to receive response from web api controllers

--- CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/services/cities.service.ts
+++ CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/services/cities.service.ts
@@ -1,20 +1,30 @@
 import { Injectable } from '@angular/core';
 import { City } from '../models/city';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CitiesService {
+  //cities: City[] = [];
+  //constructor() {
+  //  this.cities = [
+  //    new City("101", "City-A"),
+  //    new City("102", "City-B"),
+  //    new City("103", "City-C"),
+  //    new City("104", "City-D")
+  //  ];
+  //}
+  //public getCities(): City[] {
+  //  return this.cities;
+  //}
+
   cities: City[] = [];
-  constructor() {
-    this.cities = [
-      new City("101", "City-A"),
-      new City("102", "City-B"),
-      new City("103", "City-C"),
-      new City("104", "City-D")
-    ];
+  constructor(private httpClient: HttpClient) {
+
   }
-  public getCities(): City[] {
-    return this.cities;
+  public getCities(): Observable<City[]> {
+    return this.httpClient.get<City[]>("https://localhost:7283/api/v1/cities");
   }
 }
