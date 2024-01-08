981098749579105fa7e14237927edd9f6b4be7b9
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-08 19:11:05 +0530

    Modified Data annotations & PhoneNumber Property name in RegisterDTO.cs

--- CitiesManager.Core/DTO/RegisterDTO.cs
+++ CitiesManager.Core/DTO/RegisterDTO.cs
@@ -20,15 +20,15 @@ namespace CitiesManager.Core.DTO
 
         [Required(ErrorMessage = "Phone can't be blank")]
         [RegularExpression("^[0-9]*$", ErrorMessage = "Phone Number should contain only numbers")]
-        [DataType(DataType.PhoneNumber)]
-        public string Phone { get; set; } = string.Empty;
+        //[DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Password can't be blank")]
-        [DataType(DataType.Password)]
+        //[DataType(DataType.Password)]
         public string Password { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Confirm Password can't be blank")]
-        [DataType(DataType.Password)]
+        //[DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Password & Confirm Password don't match")]
         public string ConfirmPassword { get; set; } = string.Empty;
     }
