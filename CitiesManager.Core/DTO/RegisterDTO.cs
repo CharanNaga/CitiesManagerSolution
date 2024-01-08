2f33fc99284a49a1b41fb6b77e92314f5763e26c
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-08 15:18:35 +0530

    Created RegisterDTO class under DTO folder in Core Project

---
+++ CitiesManager.Core/DTO/RegisterDTO.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitiesManager.Core.DTO
+{
+    public class RegisterDTO
+    {
+        [Required(ErrorMessage = "Name can't be blank")]
+        public string PersonName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email can't be blank")]
+        [EmailAddress(ErrorMessage = "Email address should be in valid format")]
+        [Remote(action: "IsEmailAlreadyRegistered", controller: "Account", ErrorMessage = "Email already exists")] //this will dynamically add JS script validation at the email box.
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Phone can't be blank")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Phone Number should contain only numbers")]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password can't be blank")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Confirm Password can't be blank")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password & Confirm Password don't match")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
