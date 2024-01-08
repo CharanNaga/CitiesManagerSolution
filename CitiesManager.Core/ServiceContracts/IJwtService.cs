b3422fcffb2c6dcc6c6e9a5ffa1467a232ffb49f
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-08 22:40:53 +0530

    Craeted new Interface IJwtService for defining service as a resuable method for token creation

---
+++ CitiesManager.Core/ServiceContracts/IJwtService.cs
@@ -0,0 +1,16 @@
+﻿using CitiesManager.Core.DTO;
+using CitiesManager.Core.Identities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitiesManager.Core.ServiceContracts
+{
+    public interface IJwtService
+    {
+        //Based on the existing user details, generate the JWT Token & return as a response to the client along with the username, email & expiration
+        AuthenticationResponse CreateJwtToken(ApplicationUser user);
+    }
+}
