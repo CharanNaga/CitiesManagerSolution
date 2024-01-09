4bc849b10d40d54deab912a470de63eb2ca5b133
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-09 18:55:15 +0530

    Created a new method in IJwtService.cs

--- CitiesManager.Core/ServiceContracts/IJwtService.cs
+++ CitiesManager.Core/ServiceContracts/IJwtService.cs
@@ -3,6 +3,7 @@ using CitiesManager.Core.Identities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,5 +13,8 @@ namespace CitiesManager.Core.ServiceContracts
     {
         //Based on the existing user details, generate the JWT Token & return as a response to the client along with the username, email & expiration
         AuthenticationResponse CreateJwtToken(ApplicationUser user);
+
+        //Represents User details
+        ClaimsPrincipal? GetPrincipalFromJwtToken(string? token);
     }
 }
