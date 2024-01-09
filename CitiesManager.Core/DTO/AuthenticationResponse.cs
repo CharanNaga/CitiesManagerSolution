85f31c18f9fca415cd5720bf76d2fed9ca4cc27e
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-09 17:31:20 +0530

    Modified AuthenticationResponse.cs to have two additional properties

--- CitiesManager.Core/DTO/AuthenticationResponse.cs
+++ CitiesManager.Core/DTO/AuthenticationResponse.cs
@@ -12,7 +12,8 @@ namespace CitiesManager.Core.DTO
         public string? PersonName { get; set; } = string.Empty;
         public string? Email { get; set; } = string.Empty;
         public string? Token { get; set; } = string.Empty;
-
         public DateTime Expiration { get; set; }
+        public string? RefreshToken { get; set; } = string.Empty;
+        public DateTime RefreshTokenExpirationDateTime { get;set; }
     }
 }
