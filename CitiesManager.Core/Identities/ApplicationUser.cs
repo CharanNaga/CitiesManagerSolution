e019c105efdf157b61aa6173b85bd810e3662fb7
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-09 17:32:03 +0530

    Modified ApplicationUser.cs to have additional properties

--- CitiesManager.Core/Identities/ApplicationUser.cs
+++ CitiesManager.Core/Identities/ApplicationUser.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CitiesManager.Core.Identities
 {
     public class ApplicationUser : IdentityUser<Guid>
     {
         public string? PersonName { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpirationDateTime { get; set; }
     }
 }
